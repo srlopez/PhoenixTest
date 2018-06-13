8e4505ef8d646dbf816890c50c1e3cbc0fa15386
santi <santi@localhost.localdomain>
2018-06-13 15:27:50 +0200

    Minor Updates

--- lib/app_web/channels/room_channel.ex
+++ lib/app_web/channels/room_channel.ex
@@ -2,33 +2,40 @@ defmodule AppWeb.RoomChannel do
   use AppWeb, :channel
   alias AppWeb.Presence
 
-
-  def join("room:" <> _room_id, payload, socket) do
+  def join("room:" <> room_id, payload, socket) do
     if authorized?(payload) do
       send(self(), :after_join)
-      {:ok, socket}
-      #{:ok, %{channel: "room:#{room_id}"}, assign(socket, :room_id, room_id)
+      # {:ok, socket}
+      {:ok, Poison.encode!(%{channel: "room:#{room_id}"}), assign(socket, :room_id, room_id)}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
   def handle_info(:after_join, socket) do
-    user =  App.Accounts.get_user! socket.assigns[:current_user_id]
-    push socket, "presence_state", Presence.list(socket)
-    {:ok, _} = Presence.track(socket, "user:#{user.id}", %{
-      typing: false,
-      user_id: user.id,
-      name: user.name,
-      # online_at: inspect(System.system_time(:seconds))
-    })
+    user = App.Accounts.get_user!(socket.assigns.current_user_id)
+
+    # filter:Evitamos enviar otra 'presencia' que no sea la de usuarios
+    # Creo que es necesario en caso de que se 'trackeen' otros 'resources'
+    users = Enum.filter(Presence.list(socket), &Regex.match?(~r/^user:/, elem(&1, 0)))
+    push(socket, "presence_state", users)
+
+    {:ok, _} =
+      Presence.track(socket, "user:#{user.id}", %{
+        typing: false,
+        user_id: user.id,
+        name: user.name
+        # online_at: inspect(System.system_time(:seconds))
+      })
+
     {:noreply, socket}
   end
 
   def handle_in("message:add", payload, socket) do
-    #IO.inspect payload
-    App.Chat.create_message payload #%{ body: "body", name: "name", user_id: 1, room_id: 1}
-    broadcast socket, "message:new", payload
+    # IO.inspect payload
+    # %{ body: "body", name: "name", user_id: 1, room_id: 1}
+    App.Chat.create_message(payload)
+    broadcast(socket, "message:new", payload)
     {:reply, :ok, socket}
   end
 
@@ -37,11 +44,14 @@ defmodule AppWeb.RoomChannel do
     # Miedo da hacer un query a la base de dabtos cada vez que se aprieta o levanta una teclea
     # Así que paso lo que se pueda en el mensaje
 
-    {:ok, _} = Presence.update(socket, "user:#{socket.assigns[:current_user_id]}", %{
-      typing: typing,
-      user_id: socket.assigns[:current_user_id], #user.id,
-      name: name #user.name
-    })
+    {:ok, _} =
+      Presence.update(socket, "user:#{socket.assigns[:current_user_id]}", %{
+        typing: typing,
+        # user.id,
+        user_id: socket.assigns[:current_user_id],
+        # user.name
+        name: name
+      })
 
     {:reply, :ok, socket}
   end
@@ -55,7 +65,7 @@ defmodule AppWeb.RoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (room:lobby).
   def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+    broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
 
--- lib/app_web/views/layout_view.ex
+++ lib/app_web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule AppWeb.LayoutView do
   use AppWeb, :view
+
+  def js_view_name(module, template) do
+    (to_string(module) <> "_" <> to_string(template))
+    |> to_string
+    |> String.replace("View", "")
+    |> String.replace(".html", "")
+    |> String.downcase()
+    |> String.split(".")
+    |> Enum.slice(-1, 1)
+    |> to_string
+  end
 end
